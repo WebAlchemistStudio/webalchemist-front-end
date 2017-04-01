67e57bdcdf7c5dcf81b5c1b5e26101c5e81f4b2d
Jun Zheng <junzheng@exchange.niyume.com>
2017-04-01 08:01:10 -0400

    Added HTML version of privacy policy

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
         <link href="css/main.css" type="text/css" rel="stylesheet" />
         <script src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
         <script src="lib/typed.min.js"></script>
+        <script src="lib/jquery.sticky-kit.min.js"></script>
     </head>
 
     <body>
@@ -41,7 +42,7 @@
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li><a href="#"><span class='glyphicon glyphicon-home'></span> &nbsp; Home</a></li>
+                        <li><a href="?p=home"><span class='glyphicon glyphicon-home'></span> &nbsp; Home</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class='glyphicon glyphicon-console'></span> &nbsp; Services <span class="caret"></span></a>
                             <ul class="dropdown-menu">
@@ -55,129 +56,14 @@
             </div>
         </nav>
 
-        <div class="container-fluid main-container">
-            <header>
-                <div class='fade-cover'></div>
-                <div class='header-content'>
-                    <img src="images/webartisan.png" /> &nbsp; &nbsp; 
-                    <h1>Web Artisan Studio</h1><br>
-                    <p>We build websites that <span id="main-typed"></span></p>
-                </div>
-            </header>
-            <div class='sub-section money-background'>
-                <div class='fade-cover light'></div>
-                <section class='sub-heading'>
-                    One of the Most Affordble Web Design Services in Canada.
-                </section>
-                <article class='home-article'>
-                    <p>
-                        Money matters. Web Artisan Studio work hard to provide one of the most affordble web design services in Canada<br>
-                        See how we compared to other studios.
-                    </p>
-                    <br>
-                    <table class="compare-table">
-                        <tr class='heading'>
-                            <td class='other'>Studio T</td>
-                            <td><img src="images/webartisan.png" height="60" /> <br> Web Artisan Studio</td>
-                            <td class='other'>Studio L</td>
-                        </tr>
-                        <tr class="product">
-                            <td colspan="3">A landing page</td>
-                        </tr>
-                        <tr class="price">
-                            <td class="other">CAD $400</td>
-                            <td>CAD $250</td>
-                            <td class="other">CAD $700</td>
-                        </tr>
-                        <tr class="product">
-                            <td colspan="3">5 page static website</td>
-                        </tr>
-                        <tr class="price">
-                            <td class="other">CAD $1500</td>
-                            <td>CAD $750</td>
-                            <td class="other">CAD $2400</td>
-                        </tr>
-                    </table>
-                    <br><br><br><br>
-                    <p>
-                        Ready for a beautiful website?<br><br>
-                        <button class="btn btn-default btn-lg"><span class='glyphicon glyphicon-piggy-bank'></span> &nbsp; Get an Instant Estimate</button> <button class="btn btn-success btn-lg"><span class='glyphicon glyphicon-user'></span> &nbsp; Create an Account Now</button>
-                    </p>
-                </article>
-            </div>
-            <div class='sub-section mac-background'>
-                <div class='fade-cover light'></div>
-                <section class='sub-heading'>
-                    Beautiful and Mobile Friendly
-                </section>
-                <article class='home-article'>
-                    <p>We take pride in our works, we design websites that are beautiful on both desktop and mobile.</p>
-                    <img src="images/wakemeup-all.png" width="1000" />
-                    <p>
-                        In today's world, a mobile optimized website is a must. That's why all our plans includes mobile optimization for free.<br>
-                        With Web Artisan, you can be sure that your website will look beautiful everywhere.<br><br>
-                        <button class="btn btn-primary btn-lg"><span class='glyphicon glyphicon-book'></span> &nbsp; Check Out Our Portfolio</button>
-                    </p>
-                </article>
-            </div>
-            <div class='sub-section computer-background'>
-                <div class='fade-cover light'></div>
-                <section class='sub-heading'>
-                    Don't Have an IT Department? Don't Worry!
-                </section>
-                <article class='home-article'>
-                    <p>
-                        Hiring a IT person can be costly. Why not let us host your website?<br>
-                        Web Artisan Studio provides managed hosting option for all our clients. With managed hosting, you will get:
-                    </p>
-                    <ul>
-                        <li>Website designed &amp; uploaded by us.</li>
-                        <li>Global CDN for optimized loading speed across the globe.</li>
-                        <li>Free .com domain and SSL certificate.</li>
-                        <li>Premium servers operated by Microsoft Azure.</li>
-                        <li>etc...</li>
-                    </ul>
-                    <br><br>
-                    <table class="compare-table">
-                        <tr class='heading'>
-                            <td class='other'>Shared</td>
-                            <td>Managed Dedicated</td>
-                            <td class='other'>Dedicated</td>
-                        </tr>
-                        <tr class="product">
-                            <td>2 Shared Cores</td>
-                            <td>2/4/8 Cores</td>
-                            <td>2/4/8 Cores</td>
-                        </tr>
-                        <tr class="product">
-                            <td>2GB Shared RAM</td>
-                            <td>2/4/8/16 GB RAM</td>
-                            <td>2/4/8/16 GB RAM</td>
-                        </tr>
-                        <tr class="product">
-                            <td>Canada Only</td>
-                            <td>Canada, US or Japan</td>
-                            <td>Canada, US or Japan</td>
-                        </tr>
-                        <tr class="product">
-                            <td>Not Monitored</td>
-                            <td>Monitored / Managed</td>
-                            <td>Not Monitored</td>
-                        </tr>
-                        <tr class="price">
-                            <td class='other'><sub>from</sub> CAD $3/month</td>
-                            <td><sub>from</sub> CAD $20/month</td>
-                            <td class='other'><sub>from</sub> CAD $15/month</td>
-                        </tr>
-                    </table>
-                    <br>
-                    <br>
-                    <p>
-                        <button class="btn btn-primary btn-lg"><span class='glyphicon glyphicon-book'></span> &nbsp; Learn More About Hosting With Us</button>
-                    </p>
-                </article>
-            </div>
-        </div>
+        <?php
+            if(!isset($_GET["p"]) || $_GET["p"] == "home"){
+                include "pages/home.php";
+            } else if ($_GET["p"] == "privacy_policy"){
+                include "pages/privacy_policy.php";
+            }
+        ?>
+        
         <footer>
             <div class="row">
                 <div class="col-md-12" style='text-align:center;'>
@@ -187,7 +73,7 @@
             <div class="row">
                 <div class="col-md-4">
                     <h4>Policies</h4>
-                    <a href="#">Privacy Policy</a><br>
+                    <a href="?p=privacy_policy">Privacy Policy</a><br>
                     <a href="#">Refund</a><br>
                     <a href="#">Warranty</a><br>
                     <a href="#">Hosting Terms of Service</a>
@@ -213,12 +99,18 @@
     </body>
 </html>
 <script>
-  document.addEventListener('DOMContentLoaded', function(){
-      Typed.new('#main-typed', {
-        strings: ["works.", "is beautiful.", "is optimized.", "is fast to load.", "is affordble."],
-        typeSpeed: 0,
-        loop: true,
-        backDelay: 1500
-      });
-  });
+    document.addEventListener('DOMContentLoaded', function(){
+        Typed.new('#main-typed', {
+            strings: ["works.", "is beautiful.", "is optimized.", "is fast to load.", "is affordble."],
+            typeSpeed: 0,
+            loop: true,
+            backDelay: 1500
+        });
+    });
+
+    $(function(){
+        $(".stick-parent").stick_in_parent({
+            offset_top:100
+        });
+    });
 </script>
---
+++ pages/privacy_policy.php
@@ -0,0 +1,85 @@
+<div class="head-fix"></div>
+<div class="container main-container">
+    <div class='document'>
+        <br><br>
+        <h1>Privacy Policy</h1>
+        <span class='version'>v1.0 April 2017</span>
+        <div class="row">
+            <div class="col-md-3 stick-parent">
+                <ul class="hidden-xs hidden-sm document-nav">
+                    <li><a href="#">Quick Version</a></li>
+                    <li><a href="#">When?</a></li>
+                    <li><a href="#">What? Why?</a></li>
+                    <li><a href="#">What We Don't Collect</a></li>
+                    <li><a href="#">How?</a></li>
+                    <li><a href="#">You Are in Controll</a></li>
+                </ul>
+            </div>
+            <div class="col-md-9">
+                <p>
+                    Thank you for choosing Web Artisan Studio. Your privacy is our top priority, and we want you to know how do we handle it.
+                </p>
+                <h2>Quick Version</h2>
+                <p>We collect your personal information when you are using our services. We use this information to:</p>
+                <ul>
+                    <li>Contact you (Invoice, Important service announcement, etc.)</li>
+                    <li>To provide you service</li>
+                    <li>To bill you</li>
+                    <li>To analyze our website traffic</li>
+                </ul>
+                <p>We never give your personal information to third parties. All your personal information is encrypted and securely stored on our server.<br>
+                We enforce strict access policies to customer’s personal information.</p>
+                <h2>Detailed Version</h2>
+                <h3>When Do We Collect Your Information?</h3>
+                <p>We collect your personal information when:</p>
+                <ul>
+                    <li>You visit our website</li>
+                    <li>You create an account</li>
+                    <li>You are logging into our platform</li>
+                    <li>You start a project with us</li>
+                    <li>You sign up for hosting service</li>
+                    <li>We need to bill you</li>
+                </ul>
+                <h3>What Information Do We Collect?</h3>
+
+                <p><b>When you visit our website:</b>
+                When a visitor visits our website, we collect basic information about the session, such as cookies, your IP address, and your browser version.<br>
+                This information is used to analyze our website traffic only and will be deleted after 3 months.</p>
+
+                <p><b>When you create an account:</b>
+                There is some basic personal information we need when you create an account. They may include your email address, your real name, and your phone number.</p>
+
+                <p><b>When you are logging into our platform:</b>
+                Our platform uses cookies to authenticate you. Therefore, we will collect browser cookies when you are logging into our platform.</p>
+
+                <p><b>When you start a project with us / sign up for hosting service:</b>
+                We will require you to provide us your real name, and your PayPal information.</p>
+
+                <p><b>When we bill you:</b>
+                We will require you to provide us your real name, and your PayPal information.
+                Sometimes we may require you to provide your billing address as well.</p>
+
+                <h3>What Information Do We Not Collect?</h3>
+
+                <p><b>We never collect sensitive personal information such as SIN.</b> And we will never ask you to provide your personal information via email.</p>
+
+                <h3>How Do We Store Your Information?</h3>
+
+                <p>All personal information is stored on our servers across Canada and Japan. They are operated by Microsoft Azure and ConoHa. Our personal information storage system is open-source on GitHub.</p>
+
+                <p>All information is encrypted using AES-256 encryption standard, we only decrypt the information when we need them. We operate a very strict policy regarding customer’s personal information.</p>
+
+                <p>We never did, and will never share any customer’s personal information with third parties.</p>
+
+                <h3>You Are Always in Control</h3>
+
+                <p>If you have any concern about how we handle your information, please contact us at privacy@webartisanstudio.com.</p>
+
+                <p>It is necessary for us to retain some personal information to provide you service. You can always delete your account by going to account center.</p>
+
+                <p><b>This policy may change, at any time, with or without notice, we will try our best to contact you when there is a policy change in the future. (v1.0 April 2017)</b></p>
+                <br><br>
+            </div>
+        </div>
+    </div>
+</div>
