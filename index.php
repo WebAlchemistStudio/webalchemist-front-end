928112a1da7aea989d78a39b5796ee2f414e38b5
Jun Zheng <junzheng@exchange.niyume.com>
2017-04-01 05:35:01 -0400

    Added IT department section

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
                     <br><br><br><br>
                     <p>
                         Ready for a beautiful website?<br><br>
-                        <button class="btn btn-default btn-lg"><span class='glyphicon glyphicon-piggy-bank'></span> &nbsp; Get an Instant Estimate</button> <button class="btn btn-default btn-lg"><span class='glyphicon glyphicon-user'></span> &nbsp; Create an Account Now</button>
+                        <button class="btn btn-default btn-lg"><span class='glyphicon glyphicon-piggy-bank'></span> &nbsp; Get an Instant Estimate</button> <button class="btn btn-success btn-lg"><span class='glyphicon glyphicon-user'></span> &nbsp; Create an Account Now</button>
                     </p>
                 </article>
             </div>
@@ -116,16 +116,67 @@
                     <p>
                         In today's world, a mobile optimized website is a must. That's why all our plans includes mobile optimization for free.<br>
                         With Web Artisan, you can be sure that your website will look beautiful everywhere.<br><br>
-                        <button class="btn btn-default btn-lg"><span class='glyphicon glyphicon-book'></span> &nbsp; Check Out Our Portfolio</button>
+                        <button class="btn btn-primary btn-lg"><span class='glyphicon glyphicon-book'></span> &nbsp; Check Out Our Portfolio</button>
                     </p>
                 </article>
             </div>
-            <section class='sub-heading'>
-                Don't Have an IT Department? Don't Worry!
-            </section>
-            <article>
-                Don't know how to manage servers? Don't worry, we have you covered. We provide managed hosting for a price no one can beat.
-            </article>
+            <div class='sub-section computer-background'>
+                <div class='fade-cover light'></div>
+                <section class='sub-heading'>
+                    Don't Have an IT Department? Don't Worry!
+                </section>
+                <article class='home-article'>
+                    <p>
+                        Hiring a IT person can be costly. Why not let us host your website?<br>
+                        Web Artisan Studio provides managed hosting option for all our clients. With managed hosting, you will get:
+                    </p>
+                    <ul>
+                        <li>Website designed &amp; uploaded by us.</li>
+                        <li>Global CDN for optimized loading speed across the globe.</li>
+                        <li>Free .com domain and SSL certificate.</li>
+                        <li>Premium servers operated by Microsoft Azure.</li>
+                        <li>etc...</li>
+                    </ul>
+                    <br><br>
+                    <table class="compare-table">
+                        <tr class='heading'>
+                            <td class='other'>Shared</td>
+                            <td>Managed Dedicated</td>
+                            <td class='other'>Dedicated</td>
+                        </tr>
+                        <tr class="product">
+                            <td>2 Shared Cores</td>
+                            <td>2/4/8 Cores</td>
+                            <td>2/4/8 Cores</td>
+                        </tr>
+                        <tr class="product">
+                            <td>2GB Shared RAM</td>
+                            <td>2/4/8/16 GB RAM</td>
+                            <td>2/4/8/16 GB RAM</td>
+                        </tr>
+                        <tr class="product">
+                            <td>Canada Only</td>
+                            <td>Canada, US or Japan</td>
+                            <td>Canada, US or Japan</td>
+                        </tr>
+                        <tr class="product">
+                            <td>Not Monitored</td>
+                            <td>Monitored / Managed</td>
+                            <td>Not Monitored</td>
+                        </tr>
+                        <tr class="price">
+                            <td class='other'><sub>from</sub> CAD $3/month</td>
+                            <td><sub>from</sub> CAD $20/month</td>
+                            <td class='other'><sub>from</sub> CAD $15/month</td>
+                        </tr>
+                    </table>
+                    <br>
+                    <br>
+                    <p>
+                        <button class="btn btn-primary btn-lg"><span class='glyphicon glyphicon-book'></span> &nbsp; Learn More About Hosting With Us</button>
+                    </p>
+                </article>
+            </div>
         </div>
         
     </body>
