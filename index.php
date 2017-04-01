8fa67a01070196124f96e4fa6429d403617bbfd3
Jun Zheng <junzheng@exchange.niyume.com>
2017-04-01 04:47:19 -0400

    Added basic front page

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!doctype html>
-
 <html>
     <head>
         <title>Web Artisan Studio</title>
@@ -21,9 +20,117 @@
         <meta name="msapplication-TileColor" content="#ffffff">
         <meta name="msapplication-TileImage" content="images/favicons/ms-icon-144x144.png">
         <meta name="theme-color" content="#ffffff">
+        <link href="https://fonts.googleapis.com/css?family=Lato|Oswald" rel="stylesheet">
+        <link href="lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+        <link href="css/main.css" type="text/css" rel="stylesheet" />
+        <script src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+        <script src="lib/typed.min.js"></script>
     </head>
 
     <body>
+        <nav class="navbar navbar-default" id="primary-nav">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"><img src="images/webartisan.png" /></a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#"><span class='glyphicon glyphicon-home'></span> &nbsp; Home</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class='glyphicon glyphicon-console'></span> &nbsp; Services <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Website Development</a></li>
+                                <li><a href="#">Managed Hosting</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#"><span class="glyphicon glyphicon-book"></span> &nbsp; About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container-fluid main-container">
+            <header>
+                <div class='fade-cover'></div>
+                <div class='header-content'>
+                    <img src="images/webartisan.png" /> &nbsp; &nbsp; 
+                    <h1>Web Artisan Studio</h1><br>
+                    <p>We build websites that <span id="main-typed"></span></p>
+                </div>
+            </header>
+            <div class='sub-section money-background'>
+                <div class='fade-cover light'></div>
+                <section class='sub-heading'>
+                    One of the Most Affordble Web Design Services in Canada.
+                </section>
+                <article class='home-article'>
+                    <p>
+                        Money matters. Web Artisan Studio work hard to provide one of the most affordble web design services in Canada<br>
+                        See how we compared to other studios.
+                    </p>
+                    <br>
+                    <table class="compare-table">
+                        <tr class='heading'>
+                            <td class='other'>Studio T</td>
+                            <td><img src="images/webartisan.png" height="60" /> <br> Web Artisan Studio</td>
+                            <td class='other'>Studio L</td>
+                        </tr>
+                        <tr class="product">
+                            <td colspan="3">A landing page</td>
+                        </tr>
+                        <tr class="price">
+                            <td class="other">CAD $400</td>
+                            <td>CAD $250</td>
+                            <td class="other">CAD $700</td>
+                        </tr>
+                        <tr class="product">
+                            <td colspan="3">5 page static website</td>
+                        </tr>
+                        <tr class="price">
+                            <td class="other">CAD $1500</td>
+                            <td>CAD $750</td>
+                            <td class="other">CAD $2400</td>
+                        </tr>
+                    </table>
+                    <br><br><br><br>
+                    <p>
+                        Ready for a beautiful website?<br><br>
+                        <button class="btn btn-default btn-lg"><span class='glyphicon glyphicon-piggy-bank'></span> &nbsp; Get an Instant Estimate</button> <button class="btn btn-default btn-lg"><span class='glyphicon glyphicon-user'></span> &nbsp; Create an Account Now</button>
+                    </p>
+                </article>
+            </div>
+            <div class='sub-section mac-background'>
+                <div class='fade-cover light'></div>
+                <section class='sub-heading'>
+                    Beautiful and Mobile Friendly
+                </section>
+                <article class='home-article'>
+                    We take pride in our works, we design websites that are beautiful on both desktop and mobile.
+                </article>
+            </div>
+            <section class='sub-heading'>
+                Don't Have an IT Department? Don't Worry!
+            </section>
+            <article>
+                Don't know how to manage servers? Don't worry, we have you covered. We provide managed hosting for a price no one can beat.
+            </article>
+        </div>
         
     </body>
 </html>
+<script>
+  document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('#main-typed', {
+        strings: ["works.", "is beautiful.", "is optimized.", "is fast to load.", "is affordble."],
+        typeSpeed: 0,
+        loop: true,
+        backDelay: 1500
+      });
+  });
+</script>
